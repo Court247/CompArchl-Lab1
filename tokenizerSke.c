673bf9ad1235adff09e35b3c31fa1b180c019d7c
Courtney <clwoods2@miners.utep.com>
2022-02-11 21:58:29 -0700

    Did 3 methods

--- tokenizerSke.c
+++ tokenizerSke.c
@@ -1,51 +1,120 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-/* Return true (non-zero) if c is a whitespace characer
-   ('\t' or ' ').
+
+/* Return true (non-zero) if c is the delimiter character
+   previously chosen by the user.
    Zero terminators are not printable (therefore false) */
-bool delim_character(char c){
+bool delim_character(char c, char delim)
+{
+   if(c == delim){
+      return true;
+   }else{
+      return false;
+   }
 
 }
 
-/* Return true (non-zero) if c is a non-whitespace
-   character (not tab or space).
+/* Return true (non-zero) if c is a non-delimiter
+   character.
    Zero terminators are not printable (therefore false) */
-bool non_delim_character(char c){
-
+bool non_delim_character(char c, char delim)
+{
+   if (c != delim){
+      return true;
+   }else{
+      return false;
+   }
 }
 
 /* Returns a pointer to the first character of the next
-   space-separated word*/
-char *word_start(char* str){
+   word starting with a non-delimiter character. Return a zero pointer if
+   str does not contain any words.*/
+char *word_start(char* str,char delim)
+{
+   int i =0;
+   char* p = NULL;
+
+   //while string not empty
+   while(str[i] != '\0'){
+
+      //if char next to it is a non delim char
+      if (non_delim_character(str[i+1], delim)){
+
+         //equals word after delim character
+         p = &str[i+1];
+         
+         //stop loop
+         break;
+      }
+      
+      //increment i
+      i++;
+   }
+   return p;
+}
+
+/* Returns a pointer to the first delimiter character of the zero
+   terminated string*/
+char *end_word(char* str,char delim)
+{
+   int i = 0;
+   char* p = NULL;
+   //while not the end of string
+   while(str[i] != '\0'){
+      //if char next to it is a delim char
+      if(delim_character(str[i+1],delim)){
+
+         //equals word before delim char
+         p = &str[i];
+
+         //stop loop
+         break;
+      }
+      //increment i
+      i++;
+   }
+   return p;
+}
+
+/* Counts the number of words or tokens*/
+int count_tokens(char* str,char delim)
+{
+   int i = 0;
+   int c = 1;
+   // while string not empty
+   while(str[i] != '\0'){
+      //adds 1 at every delim character
+      if (delim_character(str[i], delim)){
+         c += 1;
+      }
+   }
+   //subtracts 1 due to it not counting the '\0' at the end of the word
+   return c-1;
+}
+
+/* Returns a freshly allocated new zero-terminated string
+   containing <len> chars from <inStr> */
+char *copy_str(char *inStr, short len, char delim)
+{
 
 }
 
-/* Returns a pointer to the first space character of the zero
-terminated string*/
-char *end_word(char* str){
-
-}
-// counts the number of words or tokens
-int count_tokens(char* str){
-
-
-}
 /* Returns a freshly allocated zero-terminated vector of freshly allocated
-   space-separated tokens from zero-terminated str.
-   For example, tokenize("hello world string") would result in:
+   delimiter-separated tokens from zero-terminated str.
+   For example, tokenize("hello world string"), with a character delimiter
+   of a space " " would result in:
      tokens[0] = "hello"
      tokens[1] = "world"
      tokens[2] = "string"
      tokens[3] = 0
 */
-char *copy_str(char *inStr, short len){
+char** tokenize(char* str, char delim)
+{
 
 }
 
-char** tokenize(char* str){
-}
+void print_all_tokens(char** tokens)
+{
 
-
-void print_all_tokens(char** tokens){
 }
